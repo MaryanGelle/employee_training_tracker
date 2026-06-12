a841c325b3ae661c853a5f80fe096f7f31fcff60
MaryanGelle <130154155+MaryanGelle@users.noreply.github.com>
2026-06-12 09:30:17 +0300

    changes added to menu

--- run.py
+++ run.py
@@ -1,2 +1,12 @@
 print("Welcome to Employee Training Tracker")
 print("This app helps track staff training records.")
+print("nMenu")
+print("1. Add training record")
+print("2. View records")
+print("3. Analyse records")
+print("4. Exit")
+
+choice = input("Choose an option:")
+
+print(f"You selected option {choice}.")
+print(choice)
